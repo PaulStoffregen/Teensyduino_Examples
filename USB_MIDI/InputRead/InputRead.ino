6c2b90d3b54e09bc743fb1174fc0e17c95a35f07
PaulStoffregen <paul@pjrc.com>
2018-01-08 02:02:06 -0800

    Opps, fixes to USB MIDI InputRead

--- USB_MIDI/InputRead/InputRead.ino
+++ USB_MIDI/InputRead/InputRead.ino
@@ -29,11 +29,11 @@ void processMIDI(void) {
 
   // fetch the MIDI message, defined by these 5 numbers (except SysEX)
   //
-  type = usbMIDI.getType();    // which MIDI message, 128-255
-  channel = usbMIDI.getType(); // which MIDI channel, 1-16
-  data1 = usbMIDI.getData1();  // first data byte of message, 0-127
-  data2 = usbMIDI.getData1();  // second data byte of message, 0-127
-  cable = usbMIDI.getCable();  // which virtual cable with MIDIx8, 0-7
+  type = usbMIDI.getType();       // which MIDI message, 128-255
+  channel = usbMIDI.getChannel(); // which MIDI channel, 1-16
+  data1 = usbMIDI.getData1();     // first data byte of message, 0-127
+  data2 = usbMIDI.getData2();     // second data byte of message, 0-127
+  cable = usbMIDI.getCable();     // which virtual cable with MIDIx8, 0-7
 
   // print info about the message
   //
